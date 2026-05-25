89ec18d1f1c166ac2c6e4d707d25ba68fc31c6f3
antoine <antoine.moran@protonmail.com>
2026-05-25 22:02:24 +0200

    build: drop comments, use rpath-link to avoid silencing all linker errors

--- flake.nix
+++ flake.nix
@@ -25,16 +25,11 @@
       pkgs.mkShell {
         packages = with pkgs; [
           pkg-config fvm clang cmake ninja
-          # GTK stack — in packages so nixpkgs pkg-config wrapper auto-discovers .pc files
           gtk3 glib cairo pango gdk-pixbuf libepoxy fontconfig dbus
           at-spi2-core libsysprof-capture pcre2 libffi
-          # Image libs (gdk-pixbuf/libtiff transitive)
           libtiff libdeflate lerc libjpeg libpng libwebp jbigkit xz zstd
-          # System (gio/dbus chain)
           util-linux libselinux libsepol zlib systemd expat
-          # Text rendering (pango chain)
           libthai libdatrie harfbuzz fribidi
-          # X11
           libx11 libxcursor libxrandr libxi libxdmcp libxau libxcb libxkbcommon
         ];
 
@@ -42,11 +37,12 @@
         NIX_LD_LIBRARY_PATH = lib.makeLibraryPath ([pkgs.stdenv.cc.cc] ++ runtimeLibs);
         LD_LIBRARY_PATH = lib.makeLibraryPath runtimeLibs;
 
-        LDFLAGS = "-Wl,--allow-shlib-undefined " + lib.concatMapStringsSep " " (p: "-L${p}/lib") (with pkgs; [
-          libepoxy fontconfig.lib gtk3 glib.out pango.out cairo gdk-pixbuf
-          libselinux.out libsepol.out libthai libdatrie.out harfbuzz fribidi
-          libxdmcp libxau libxcb
-        ]);
+        LDFLAGS = "-Wl,-rpath-link," + lib.makeLibraryPath runtimeLibs + " "
+          + lib.concatMapStringsSep " " (p: "-L${p}/lib") (with pkgs; [
+            libepoxy fontconfig.lib gtk3 glib.out pango.out cairo gdk-pixbuf
+            libselinux.out libsepol.out libthai libdatrie.out harfbuzz fribidi
+            libxdmcp libxau libxcb
+          ]);
       };
   };
 }
