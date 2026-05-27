730a09e3bfc00ef9fba8ef3766f36d73648e96d5
Alwenyfae <62889303+alwenyfae@users.noreply.github.com>
2026-05-27 18:05:05 +0200

    updated notification scheduler

--- lib/controllers/notification_scheduler.dart
+++ lib/controllers/notification_scheduler.dart
@@ -43,49 +43,6 @@ class NotificationScheduler {
       return;
     }
 
-    // final timeFormat = DateFormat.Hm(localeName);
-    // final weekdayFormatter = DateFormat.EEEE(localeName);
-
-    // final List<Future<void>> schedulingFutures = [];
-
-    // for (final schedule in medicationScheduleProvider.schedules) {
-    //   final lastTaken = medicationIntakeProvider
-    //       .getLastIntakeLocalDateForSchedule(schedule.id);
-
-    //   for (final dayOfWeek in schedule.daysOfWeek) {
-    //     // Use Jan 1, 2024 (a Monday) as a reference to get the weekday name
-    //     final referenceDate = DateTime(2024, 1, 1 + (dayOfWeek - 1));
-    //     final weekdayName = weekdayFormatter.format(referenceDate);
-
-    //     for (final time in schedule.notificationTimes) {
-    //       final timeStr =
-    //           timeFormat.format(DateTime(2024, 1, 1, time.hour, time.minute));
-
-    //       // Calculate startDate for this repeating notification
-    //       DateTime start = schedule.startDate.toDateTime();
-
-    //       // If today matches this dayOfWeek and it's already taken, start from tomorrow to skip today
-    //       if (Date.today().weekday == dayOfWeek &&
-    //           schedule.isTakenTodayOrLater(lastTaken)) {
-    //         start = DateTime.now().add(const Duration(days: 1));
-    //       }
-
-    //       schedulingFutures.add(
-    //         NotificationService().scheduleWeeklyNotification(
-    //           title: l10n.notificationMedicationReminderTitle(schedule.name),
-    //           body: l10n.notificationMedicationReminderBody(
-    //             "$weekdayName $timeStr",
-    //           ),
-    //           dayOfWeek: dayOfWeek,
-    //           hour: time.hour,
-    //           minute: time.minute,
-    //           startDate: start,
-    //         ),
-    //       );
-    //     }
-    //   }
-    // }
-    // await Future.wait(schedulingFutures);
     final scheduledDateFormat = DateFormat.MMMMd(localeName);
     final scheduledDateTimeFormat = DateFormat.MMMMd(localeName)
         .addPattern(DateFormat.Hm(localeName).pattern);
