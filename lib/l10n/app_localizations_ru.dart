f21a2bc32df070838c39afab81f0f77dc7fbadba
Alwenyfae <62889303+alwenyfae@users.noreply.github.com>
2026-05-27 17:30:46 +0200

    fixed ru language (used llm for translation)

--- lib/l10n/app_localizations_ru.dart
+++ lib/l10n/app_localizations_ru.dart
@@ -86,6 +86,9 @@ class AppLocalizationsRu extends AppLocalizations {
   String get scheduleFrequencyInterval => 'Интервально';
 
   @override
+  String get scheduleFrequencyWeekly => 'Еженедельно';
+
+  @override
   String get newUpdateAvailable => 'Доступно новое обновление!';
 
   @override
