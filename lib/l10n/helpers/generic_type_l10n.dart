a89cc5cb6bc72183bf1869279f8c0f94c878f187
Délia Cheminot <delia.cheminot@gmail.com>
2026-06-01 00:03:18 +0200

    refactor: simplify generic type l10n

--- lib/l10n/helpers/generic_type_l10n.dart
+++ lib/l10n/helpers/generic_type_l10n.dart
@@ -2,27 +2,24 @@ import 'package:mona/data/model/generic_supply_item.dart';
 import 'package:mona/l10n/app_localizations.dart';
 
 extension GenericTypeL10n on GenericSupplyType {
-  String localizedName(AppLocalizations localizations, [num count = 1]) =>
-      _GenericTypeDisplayNames.resolve(this, localizations, count);
-}
-
-abstract final class _GenericTypeDisplayNames {
-  static final Map<String, String Function(AppLocalizations, num)>
-      _labelsByName = {
-    GenericSupplyType.syringe.name: (l, c) => l.syringe(c),
-    GenericSupplyType.wipe.name: (l, c) => l.wipe(c),
-    GenericSupplyType.needle.name: (l, c) => l.needle(c),
-    GenericSupplyType.gloves.name: (l, c) => l.gloves(c),
-    GenericSupplyType.bandage.name: (l, c) => l.bandage(c),
-  };
-
-  static String resolve(
-      GenericSupplyType type, AppLocalizations localizations, num count) {
-    final labelBuilder = _labelsByName[type.name];
-    if (labelBuilder != null) {
-      return labelBuilder(localizations, count);
+  String localizedName(AppLocalizations l10n, [num count = 1]) {
+    switch (this) {
+      case GenericSupplyType.syringe:
+        return l10n.syringe(count);
+      case GenericSupplyType.wipe:
+        return l10n.wipe(count);
+      case GenericSupplyType.needle:
+        return l10n.needle(count);
+      case GenericSupplyType.gloves:
+        return l10n.gloves(count);
+      case GenericSupplyType.bandage:
+        return l10n.bandage(count);
     }
-    final n = type.name;
-    return n[0].toUpperCase() + n.substring(1);
+  }
+
+  String fallbackLabel() {
+    final name = this.name;
+    if (name.isEmpty) return name;
+    return name[0].toUpperCase() + name.substring(1);
   }
 }
