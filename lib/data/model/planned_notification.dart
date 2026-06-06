e87198143f541cf6fe21893dcff21748ca02b168
Délia Cheminot <delia.cheminot@gmail.com>
2026-06-06 15:54:47 +0200

    feat: add planned notification type

---
+++ lib/data/model/planned_notification.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:mona/data/model/medication_schedule.dart';
+
+@immutable
+sealed class PlannedNotification {
+  final MedicationSchedule schedule;
+
+  const PlannedNotification(this.schedule);
+}
+
+class PlannedOccurrence extends PlannedNotification {
+  final DateTime dateTime;
+
+  const PlannedOccurrence(super.schedule, {required this.dateTime});
+}
+
+enum Periodicity { daily, weekly }
+
+class PlannedRepeating extends PlannedNotification {
+  final Periodicity periodicity;
+  final DateTime firstFire;
+  final TimeOfDay time;
+
+  final int? dayOfWeek;
+
+  const PlannedRepeating(
+    super.schedule, {
+    required this.periodicity,
+    required this.firstFire,
+    required this.time,
+    this.dayOfWeek,
+  }) : assert(
+          (periodicity == Periodicity.weekly) == (dayOfWeek != null),
+          'dayOfWeek must be set iff periodicity == weekly',
+        );
+}
