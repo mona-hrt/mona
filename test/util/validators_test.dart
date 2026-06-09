442022ff54aa20c060008d5a1141f887bb6209f9
Délia Cheminot <delia.cheminot@gmail.com>
2026-06-09 10:13:17 +0200

    chore: remove unused import

--- test/util/validators_test.dart
+++ test/util/validators_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mona/data/model/administration_route.dart';
 import 'package:mona/data/model/molecule.dart';
