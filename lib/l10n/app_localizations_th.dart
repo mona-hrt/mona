6b3ff6afbbab2525e0d65234876d3ea110a811cf
itscrystalline <crystal@iw2tryhard.dev>
2026-06-14 21:57:04 +0700

    feat: translate generic counter words and units

--- lib/l10n/app_localizations_th.dart
+++ lib/l10n/app_localizations_th.dart
@@ -140,8 +140,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get exactRemindersDisabled => 'การแจ้งเตือนแบบตรงเวลาได้ปิดอยู่';
 
   @override
-  String get remindersDelayed =>
-      'การแจ้งเตือนอาจล่าช้าได้ แตะเพื่อเปิดการตั้งค่า';
+  String get remindersDelayed => 'การแจ้งเตือนอาจล่าช้าได้ แตะเพื่อเปิดการตั้งค่า';
 
   @override
   String get autoUpdate => 'อัพเดตอัตโนมัติ';
@@ -153,8 +152,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get checkForUpdates => 'ตรวจหาอัพเดตใหม่';
 
   @override
-  String get checkForUpdatesDescription =>
-      'ฉันจะตรวจหาอัพเดตใหม่ล่าสุดเอง\nจะทำให้เปิดเครือข่าย\n(จะไม่มีข้อมูลใดๆส่งออกนอกเครื่อง)';
+  String get checkForUpdatesDescription => 'ฉันจะตรวจหาอัพเดตใหม่ล่าสุดเอง\nจะทำให้เปิดเครือข่าย\n(จะไม่มีข้อมูลใดๆส่งออกนอกเครื่อง)';
 
   @override
   String appVersion(Object version) {
@@ -178,8 +176,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get importDataSubtitle => 'กู้คืนข้อมูลจากแบ็คอัพ JSON';
 
   @override
-  String get importDataOverwriteWarning =>
-      'จะแทนที่ข้อมูลทั้งหมดด้วยข้อมูลจากแบ็คอัพ โดยที่ย้อนกลับไม่ได้ ทำต่อ?';
+  String get importDataOverwriteWarning => 'จะแทนที่ข้อมูลทั้งหมดด้วยข้อมูลจากแบ็คอัพ โดยที่ย้อนกลับไม่ได้ ทำต่อ?';
 
   @override
   String get importConfirm => 'นำเข้า';
@@ -188,8 +185,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get importSuccessfulTitle => 'นำเข้าสำเร็จ';
 
   @override
-  String get importRestartRequired =>
-      'โปรดปิดแล้วเปิดแอพใหม่เพื่อให้ข้อมูลที่กู้คืนมาแสดงผล';
+  String get importRestartRequired => 'โปรดปิดแล้วเปิดแอพใหม่เพื่อให้ข้อมูลที่กู้คืนมาแสดงผล';
 
   @override
   String get closeApp => 'ปิดแอพ';
@@ -235,8 +231,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get updateDownloadAndInstall => 'ดาวน์โหลดและติดตั้ง';
 
   @override
-  String get updateInstallPermissionRequired =>
-      'ต้องอนุญาตการติดตั้งอัพเดตก่อน';
+  String get updateInstallPermissionRequired => 'ต้องอนุญาตการติดตั้งอัพเดตก่อน';
 
   @override
   String get updateDownloadingTitle => 'กำลังดาวน์โหลดอัพเดต…';
@@ -247,8 +242,7 @@ class AppLocalizationsTh extends AppLocalizations {
   }
 
   @override
-  String get updateDownloadFailed =>
-      'ดาวน์โหลดล้มเหลว โปรดตรวจสอบเครือข่ายของคุณ';
+  String get updateDownloadFailed => 'ดาวน์โหลดล้มเหลว โปรดตรวจสอบเครือข่ายของคุณ';
 
   @override
   String notificationMedicationReminderTitle(Object scheduleName) {
@@ -331,8 +325,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get notificationsUpdated => 'การแจ้งเตือนอัพเดตแล้ว';
 
   @override
-  String get notificationsUpdatedDescription =>
-      'ทุกตารางเวลามีการแจ้งเตือนของตนเองแล้ว\n\nโปรดตั้งการแจ้งเตือนของตารางเวลาของคุณเพื่อที่จะไม่ลืม';
+  String get notificationsUpdatedDescription => 'ทุกตารางเวลามีการแจ้งเตือนของตนเองแล้ว\n\nโปรดตั้งการแจ้งเตือนของตารางเวลาของคุณเพื่อที่จะไม่ลืม';
 
   @override
   String get dontShowAgain => 'อย่าแสตงอีก';
@@ -357,6 +350,12 @@ class AppLocalizationsTh extends AppLocalizations {
 
   @override
   String get amount => 'ปริมาณ';
+
+  @override
+  String get takenAmount => 'Taken amount';
+
+  @override
+  String get wastedAmount => 'Wasted amount';
 
   @override
   String get none => 'ไม่มี';
@@ -388,14 +387,16 @@ class AppLocalizationsTh extends AppLocalizations {
   String get microliters => 'ไมโครลิตร';
 
   @override
+  String get milliliters => 'mL';
+
+  @override
   String get empty_levels => 'การฉีดเอสตราไดออลจะขึ้นในแท็บนี้';
 
   @override
   String get bloodTestsTitle => 'การตรวจเลือด';
 
   @override
-  String get empty_blood_tests =>
-      'ผลตรวจเลือดจะมาขึ้นที่นี่ เรื่มด้วยการกดปุ่ม “เพื่ม”';
+  String get empty_blood_tests => 'ผลตรวจเลือดจะมาขึ้นที่นี่ เรื่มด้วยการกดปุ่ม “เพื่ม”';
 
   @override
   String get addBloodTest => 'เพิ่มผลตรวจเลือด';
@@ -494,27 +495,27 @@ class AppLocalizationsTh extends AppLocalizations {
 
   @override
   String syringeRemaining(num count) {
-    return 'TODO $count';
+    return '$count กระบอก';
   }
 
   @override
   String wipeRemaining(num count) {
-    return 'TODO $count';
+    return '$count ผืน';
   }
 
   @override
   String needleRemaining(num count) {
-    return 'TODO $count';
+    return '$count เข็ม';
   }
 
   @override
   String glovesRemaining(num count) {
-    return 'TODO $count';
+    return '$count ถุง';
   }
 
   @override
   String bandageRemaining(num count) {
-    return 'TODO $count';
+    return '$count ชิ้น';
   }
 
   @override
@@ -629,8 +630,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get medicationEstradiolBenzoate => 'เอสตราไดออล เบนโซเอต';
 
   @override
-  String get medicationEstradiolCypionateSuspension =>
-      'สารแขวนลอยในเอสตราไดออล ไซพิโอเนต';
+  String get medicationEstradiolCypionateSuspension => 'สารแขวนลอยในเอสตราไดออล ไซพิโอเนต';
 
   @override
   String get medicationTestosteroneEnanthate => 'เทสโทสเตอโรน เอนันเทต';
@@ -648,8 +648,7 @@ class AppLocalizationsTh extends AppLocalizations {
   String get medicationTestosteroneBenzoate => 'เทสโทสเตอโรน เบนโซเอต';
 
   @override
-  String get medicationTestosteroneCypionateSuspension =>
-      'สารแขวนลอยในเทสโทสเตอโรน ไซพิโอเนต';
+  String get medicationTestosteroneCypionateSuspension => 'สารแขวนลอยในเทสโทสเตอโรน ไซพิโอเนต';
 
   @override
   String get injection => 'ยาฉีด';
@@ -679,19 +678,19 @@ class AppLocalizationsTh extends AppLocalizations {
   String get transdermalDrops => 'ยาหยอดซึมผิวหนัง';
 
   @override
-  String get unitMilligram => 'TODO';
+  String get unitMilligram => 'มิลลิกรัม';
 
   @override
-  String get unitPgPerMl => 'TODO';
+  String get unitPgPerMl => 'พิโกกรัม/มิลลิลิตร';
 
   @override
-  String get unitPmolPerL => 'TODO';
+  String get unitPmolPerL => 'พิโกโมล/มิลลิลิตร';
 
   @override
-  String get unitNgPerDl => 'TODO';
+  String get unitNgPerDl => 'นาโนกรัม/เดซิลิตร';
 
   @override
-  String get unitNmolPerL => 'TODO';
+  String get unitNmolPerL => 'นาโนโมล/ลิตร';
 
   @override
   String administrationRouteUnitMl(num count) {
