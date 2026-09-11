fb9aeee3c7886c0a86143245bc9dade02cdb2db8
Délia Cheminot <delia.cheminot@gmail.com>
2026-09-12 01:45:18 +0200

    chore: remove unused imports

--- lib/ui/views/levels/main_graph_page/chart_buttons.dart
+++ lib/ui/views/levels/main_graph_page/chart_buttons.dart
@@ -1,13 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:m3e_core/m3e_core.dart';
-import 'package:mona/data/model/supply_item.dart';
-import 'package:mona/data/providers/supply_item_provider.dart';
 import 'package:mona/i18n/translations.g.dart';
-import 'package:mona/ui/constants/dimensions.dart';
-import 'package:mona/ui/views/supplies/supply_item_card.dart';
-import 'package:mona/ui/widgets/main_page_wrapper.dart';
-import 'package:provider/provider.dart';
 
 class ChartButtons extends StatefulWidget {
   final int index;
